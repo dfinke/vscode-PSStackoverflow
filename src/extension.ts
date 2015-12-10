f04beecbdec0a595e46d185bf486cd5decbe6e93
dfinke <finked@hotmail.com>
2015-12-10 17:18:18 -0500

    Updated

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,20 @@
+/// <reference path="../typings/open/open.d.ts" />`
+
 import * as vscode from 'vscode'; 
+import open = require('open');
 
 export function activate(context: vscode.ExtensionContext) {
+
 	console.log('Congratulations, your extension "vscode-PSStackoverflow" is now active!'); 
 
 	var disposable = vscode.commands.registerCommand('PowerShell.SearchSO', () => {
-		vscode.window.showInformationMessage('PowerShell.SearchSO!');
+		
+		const editor = vscode.window.activeTextEditor;        
+        var text = editor.document.getText(editor.selection);
+						 
+		var url = 'http://stackoverflow.com/search?q=powershell]+'+text;		 
+		 
+		open(url);
 	});
 	
 	context.subscriptions.push(disposable);
